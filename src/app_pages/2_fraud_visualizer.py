7b0003013bbb05692b69de32c16d37d19683c5b6
BlvckKryptonite <mumathedeveloper@gmail.com>
2026-01-01 00:10:11 +0200

    Add fraud visualizer page with interactive plots

---
+++ src/app_pages/2_fraud_visualizer.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("../data/cleaned_transactions.csv")
+
+
+def app():
+    st.title("Fraud Visualizer")
+    df = load_data()
+
+    # Fraud rate by hour
+    st.write("#### Fraud Rate by Hour")
+    fig, ax = plt.subplots()
+    sns.barplot(x="Hour", y="Is_Fraud", data=df, ax=ax)
+    st.pyplot(fig)
+
+    # Transaction amount distribution
+    st.write("#### Transaction Amount Distribution")
+    fig, ax = plt.subplots()
+    sns.boxplot(x="Is_Fraud", y="Amount", data=df, ax=ax)
+    st.pyplot(fig)
