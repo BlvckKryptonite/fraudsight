49b0b3978c5f098616f74322ba585fefe031aca9
BlvckKryptonite <mumathedeveloper@gmail.com>
2026-01-02 16:23:20 +0200

    Add fraud detector page for transaction predictions

---
+++ src/app_pages/3_fraud_detector.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+@st.cache_resource
+def load_model():
+    with open("../src/utils/model_pipeline.pkl", "rb") as f:
+        return pickle.load(f)
+
+
+def app():
+    st.title("Fraud Detector")
+    model = load_model()
+
+    uploaded_file = st.file_uploader("Upload transactions (CSV)")
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file)
+        predictions = model.predict(df)
+        df["Prediction"] = predictions
+        st.write(df[["Transaction_ID", "Amount", "Prediction"]])
